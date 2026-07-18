c2b3a897950ee2b0d5192954f4bc1cceadb60bcd
lovish0001 <7696859022garg@gmail.com>
2026-07-18 14:59:01 +0530

    StrongNumbers.java solution

---
+++ Assignment-01/StrongNumbers.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class StrongNumbers {
+
+    // Method to calculate factorial
+    public static int factorial(int n) {
+        int fact = 1;
+
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+
+        return fact;
+    }
+
+    // Method to check Strong Number
+    public static boolean isStrong(int num) {
+        int original = num;
+        int sum = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            sum += factorial(digit);
+            num /= 10;
+        }
+
+        return sum == original;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Start: ");
+        int start = sc.nextInt();
+
+        System.out.print("Enter End: ");
+        int end = sc.nextInt();
+
+        System.out.println("Strong Numbers:");
+
+        for (int i = start; i <= end; i++) {
+            if (isStrong(i)) {
+                System.out.println(i);
+            }
+        }
+
+        sc.close();
+    }
+}
